866db9ca5dad8aab98c6ccc51654b59491d2e66e
Vladislav Sokolov <vladislav.sokolov92@gmail.com>
2019-01-06 23:32:07 +0200

    update index.js

--- a-tiny-JS-world/index.js
+++ a-tiny-JS-world/index.js
@@ -5,10 +5,60 @@
    Web app: _put project's github pages URL here_
    */
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+// ======== objectECTS DEFINITIONS ========
+// Define your objectects here
+const man = {
+    species: 'human',
+    name:'Dima',
+    legs:2,
+    hands:2,
+    gender:'male',
+    saying:`I live in Kyiv`,
+};
 
+const woman ={
+    species:'human',
+    name:'Viktoria',
+    legs:2,
+    hands:2,
+    gender: 'female',
+    saying:'I live in Dnipro',
+};
 
+const cat = {
+    species: 'cat',
+    name: 'Mark',
+    legs: 4,
+    hands: 0,
+    gender: 'male',
+    saying: 'may-may',
+};
+
+const dog = {
+    species: 'dog',
+    name: 'Lola',
+    legs: 4,
+    hands : 0,
+    gender:'female',
+    saying: 'gav-gav',
+};
+
+const catwoman ={
+    species:'catwoman',
+    name:'Catwoman',
+    legs:2,
+    hands:2,
+    gender:'female',
+    saying: cat.saying,
+}
+
+function toString(object){
+    return [object.species, object.name, object.gender, object.legs, object.hands, object.saying + ';'].join ('; ');
+}
+
+[man, woman, cat, dog, catwoman].forEach(inhabitant =>
+    print(toString(inhabitant))
+);
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -27,5 +77,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
